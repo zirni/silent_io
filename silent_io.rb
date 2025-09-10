afbee5cd9546a7d1a4e591e544e4988eff849c3e
Matthias Zirnstein <matthias.zirnstein@gmail.com>
2025-09-10 08:52:14 +0200

    init

---
+++ silent_io.rb
@@ -0,0 +1,12 @@
+class SilentIO
+  def self.call
+    original_stdout = $stdout
+    original_stderr = $stderr
+    $stdout = StringIO.new
+    $stderr = StringIO.new
+    yield
+  ensure
+    $stdout = original_stdout
+    $stderr = original_stderr
+  end
+end
---
+++ spec/silent_io_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../silent_io'
+describe SilentIO do
+  it "silences $stdout" do
+    expect(true).to eq(true)
+
+    expect($stdout).to_not receive(:puts).with('foo')
+
+    SilentIO.call do
+      puts 'foo'
+    end
+
+  end
+
+  it "silences $stderr" do
+    expect(true).to eq(true)
+
+    expect($stderr).to_not receive(:puts).with('bar')
+
+    SilentIO.call do
+      $stderr.puts 'bar'
+    end
+
+  end
+end
